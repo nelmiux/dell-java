a1824ec9869744060ace43159865b381081bf79d
Perera <Nelma_Perera@Dell.com>
2018-12-14 15:47:22 -0600

    console greetings added

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,8 +1,12 @@
-
+import java.util.Scanner;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String string1 = "Nelma";
+	    System.out.println("Hi, my name is " + string1);
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("what's your name?");
+	    String input = sc.nextLine();
+	    System.out.println("Nice to meet you, " + input);
 	}
 }
